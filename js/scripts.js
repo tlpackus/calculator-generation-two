701d7f8a19179bb7c76efbb1f3dad018c374642d
tlpackus <tlpackus@gmail.com>
2021-01-29 21:51:38 -0800

    update the scripts.js buisness and userinterface logic typing with the lessons examples. now its a fully functioning calc

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+const add = function(number1, number2) {
+  return number1 + number2;
+};
+
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
+});
